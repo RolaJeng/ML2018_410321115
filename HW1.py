23eec99d2f1f44880072328b4b32b27d6007ef25
ROLA-PC\RolaJeng <trionlaa15@gmail.com>
2018-04-27 15:58:46 +0800

    Add0427
    新增版本 20180427

--- HW1.py
+++ HW1.py
@@ -1,11 +1,14 @@
-import numpy
+import numpy as np
 import cv2
-from PIL import Image
+
+imgKey1 = cv2.imread("key1.png")
+imgKey2 = cv2.imread("key2.png")
+imgI = cv2.imread("I.png")
+imgE = cv2.imread("E.png")
+imgEprime = cv2.imread("Eprime.png")
 
 
-im = Image.open( "E.png" );
-widthA , heightA = im.size
 
-Epoch = 1;
 
-print(Epoch);
+Epoch = 1
+
