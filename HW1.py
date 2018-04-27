c1d88af1ea4789787558eb9713f9f181b1da8872
ROLA-PC\RolaJeng <trionlaa15@gmail.com>
2018-04-27 17:08:04 +0800

    20180427_1706
    新增公式回全圈

--- HW1.py
+++ HW1.py
@@ -1,14 +1,32 @@
 import numpy as np
 import cv2
 
+epoch = 1
+
 imgKey1 = cv2.imread("key1.png")
 imgKey2 = cv2.imread("key2.png")
 imgI = cv2.imread("I.png")
 imgE = cv2.imread("E.png")
-imgEprime = cv2.imread("Eprime.png")
+imgEprime = cv2.imread('Eprime.png')
+
+#設定初始權重
+output = [1, 1, 1]
+#圖片的長寬
+height, width, channels = imgKey1.shape
+
+#將每個pixel照演算法的公式去做推理W的動作
+arrayA = np.zeros((height, width, 3), np.uint8)
+arrayE = np.zeros((height, width, 3), np.uint8)
+arrayI = np.zeros((height, width, 3), np.uint8)
+r = 0.00001
+
+while (epoch < 2):
+    for i in range(1, width):
+        for j in range(1, height):
+            arrayA = output
 
 
 
 
-Epoch = 1
+
 
