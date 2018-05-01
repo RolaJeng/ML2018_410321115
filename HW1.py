12f3ee23cfd1348f59e975d370f2f4cf74fa14c4
ROLA-PC\RolaJeng <trionlaa15@gmail.com>
2018-05-01 19:49:10 +0800

    initial matrix
    initial matrix arrayA, arrayE, arrayI

--- HW1.py
+++ HW1.py
@@ -9,24 +9,34 @@ imgI = cv2.imread("I.png")
 imgE = cv2.imread("E.png")
 imgEprime = cv2.imread('Eprime.png')
 
-#設定初始權重
-output = [1, 1, 1]
-#圖片的長寬
-height, width, channels = imgKey1.shape
+#initial Weights of w
+w = np.array([1, 1, 1])
+w.transpose()
 
-#將每個pixel照演算法的公式去做推理W的動作
-arrayA = np.zeros((height, width, 3), np.uint8)
-arrayE = np.zeros((height, width, 3), np.uint8)
-arrayI = np.zeros((height, width, 3), np.uint8)
-r = 0.00001
+#height and width of Image
+size = imgKey1.shape
 
-while (epoch < 2):
-    for i in range(1, width):
-        for j in range(1, height):
-            arrayA = output
+height = size[0]
+width = size[1]
+pixelsValue = size[2]
 
+limit = 2
+alpha = 0.00001
+#initial value of matrix
+arrayA = np.zeros(0)
+arrayE = np.zeros(0)
+arrayI = np.zeros(0)
 
+while (epoch < limit ):
+    for i in range(1, height):
+        for j in range(1, width):
+            arrayA = np.transpose(w) * np.transpose([imgKey1, imgKey2, imgI])
+            arrayE = imgE - arrayA
+            w = w + alpha * arrayE * np.transpose([imgKey1, imgKey2, imgI])
+    epoch = epoch + 1
 
+#for i in range(1, width):
+   # for j in range(1, height):
+    #    arrayI = imgEprime - w(1) * imgKey1 - w(2) * imgKey2 / w(3)
 
-
-
+#cv2.imshow(imgI)
