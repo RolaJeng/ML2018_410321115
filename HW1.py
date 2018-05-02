9babf1b753655fb626e2a58ca4b963cbd5bb2658
ROLA-PC\RolaJeng <trionlaa15@gmail.com>
2018-05-02 23:19:48 +0800

    fix the error
    * convert images to gray level
    * recoding the tranning algorithm
    * finish the project

--- HW1.py
+++ HW1.py
@@ -1,56 +1,69 @@
 import numpy as np
 import cv2
 
+#read the images
 imgKey1 = cv2.imread("key1.png")
 imgKey2 = cv2.imread("key2.png")
 imgI = cv2.imread("I.png")
 imgE = cv2.imread("E.png")
 imgEprime = cv2.imread('Eprime.png')
 
-arrK1 = np.array(imgKey1)
-arrK2 = np.array(imgKey2)
-arrI = np.array(imgI)
-arrE = np.array(imgE)
-arrEp = np.array(imgEprime)
+#convert images to gray_level
+grK1 = cv2.cvtColor(imgKey1, cv2.COLOR_BGR2GRAY)
+grK2 = cv2.cvtColor(imgKey2, cv2.COLOR_BGR2GRAY)
+grI = cv2.cvtColor(imgI, cv2.COLOR_BGR2GRAY)
+grE = cv2.cvtColor(imgE, cv2.COLOR_BGR2GRAY)
+grEp = cv2.cvtColor(imgEprime, cv2.COLOR_BGR2GRAY)
 
-outImg = np.zeros(imgEprime.shape, np.uint8)
+#create matrix to the gray images
+arrK1 = np.array(grK1)
+arrK2 = np.array(grK2)
+arrI = np.array(grI)
+arrE = np.array(grE)
+arrEp = np.array(grEp)
+outputImg = np.zeros(arrEp.shape, np.uint8)
 
+#read the size of input image
 size = imgKey1.shape
 height = size[0]
 width = size[1]
 
+#initialize
 epoch = 1
 w = np.random.rand(3)
-w = np.transpose(w)
+x = np.random.rand(3)
+e = np.random.rand(3)
+maxIterLimit = 10
+alp = 0.000001
+vig = 0.000001
 
-maxIterLimit = 2 #max number of epochs
-alp = 0.00001 #learning rate
-
-a = np.zeros((height, width))
-e = np.zeros((height, width))
-x = np.zeros((height, width))
-output = np.zeros((height, width))
-
-while (epoch == 1 or epoch < maxIterLimit ):
-    for i in range(0, width - 1):
-        for j in range(0, height - 1):
-            x = [arrK1[j][i], arrK2[j][i], arrI[j][i]]
-            x = np.transpose(x)
-            w = np.transpose(w)
-            a = w * x
+#tranning algorithm
+while (epoch < maxIterLimit and abs(w[1] - w[0]) > vig):
+    for i in range(1, width):
+        for j in range(1, height):
+            tmpK1 = arrK1[j][i]
+            tmpK2 = arrK2[j][i]
+            tmpI = arrI[j][i]
+            x[0] = tmpK1
+            x[1] = tmpK2
+            x[2] = tmpI
+            a = np.dot(w, x)
             e = arrE[j][i] - a
             w = w + alp * e * x
     epoch = epoch + 1
 
-for i in range(0, width - 1):
-    for j in range(0, height - 1):
+#find the value of w by using the formula
+for i in range(1, width):
+    for j in range(1, height):
         output = (arrEp[j][i] - w[0] * arrK1[j][i] - w[1] * arrK2[j][i]) / w[2]
-        outImg[j][i] = output
+        if np.any(output > 255):
+            output = 255
+        if np.any(output < 0):
+            output = 0
+        outputImg[j][i] = output
 
-print(w[0], w[1], w[2])
-
-cv2.imshow('Decryption Image', outImg,)
-cv2.imwrite('Decryption Image.png', outImg)
-
+print(w)
+cv2.imwrite('Output Image.png', outputImg)
+cv2.imshow('Output Image.png', outputImg)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
+
