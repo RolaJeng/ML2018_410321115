6a0937228ec286970f3ab0a4d901808253a19c67
ROLA-PC\RolaJeng <trionlaa15@gmail.com>
2018-07-05 01:03:53 +0800

    add classifier

--- Homework 2/handwritten.py
+++ Homework 2/handwritten.py
@@ -1,25 +1,13 @@
 from sklearn.datasets import fetch_mldata
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import  train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import classification_report
 from sklearn import svm
-import time
+from sklearn.neighbors import KNeighborsClassifier
 
-mnist = fetch_mldata('MNIST original')
-mnData = mnist['data']
-mnTarget = mnist['target']
-
-mnTarget = mnTarget.astype("int32")
-mnData = mnData / 255.0
-mnData.min(), mnData.max()
-
-trainData, testData, trainTarget, testTarget = train_test_split(mnData, mnTarget)
-trainData.shape, testData.shape
-#print(mnData.dtype, mnTarget.dtype)
-#print(mnData.shape, mnTarget.shape)
-
+#digit recognition output images label
 def pltImages(images, labels):
     cols = min(5, len(images))
     rows = len(images) // cols
@@ -33,20 +21,54 @@ def pltImages(images, labels):
     plt.show()
 
 #train model using Naive Bayes
-#cls = MultinomialNB()
-#cls.fit(trainData, trainTarget)
-#evaluate model
-#cls.score(testData, testTarget)
+def naiveBayes(trainData, testData, trainTarget, testTarget):
+    nb_clf = MultinomialNB()
+    nb_clf.fit(trainData, trainTarget)
+    nb_acc = nb_clf.score(testData, testTarget)
+    nb_acc = "%.4f" % nb_acc
+    print("Accuracy of Naive Bayes: " + str(nb_acc))
+    predictions = nb_clf.predict(testData)
+    print(classification_report(testTarget, predictions))
+    p = np.random.permutation(len(testData))
+    p = p[:20]
+    pltImages(testData[p].reshape(-1, 28, 28), predictions[p])
 
-svm_clf = svm.SVC()
-svm_clf.fit(trainData, trainTarget)
-svm_clf.score(testData, testTarget)
-predictions = svm_clf.predict(testData)
+#train model using SVM
+def SVM(trainData, testData, trainTarget, testTarget):
+    svm_clf = svm.SVC()
+    svm_clf.fit(trainData, trainTarget)
+    svm_acc = svm_clf.score(testData, testTarget)
+    svm_acc = "%.4f" % svm_acc
+    print("Accuracy of SVM: " + str(svm_acc))
+    predictions = svm_clf.predict(testData)
+    print(classification_report(testTarget, predictions))
+    p = np.random.permutation(len(testData))
+    p = p[:20]
+    pltImages(testData[p].reshape(-1, 28, 28), predictions[p])
 
-#predictions = cls.predict(testData)
+#train model using Nearest Neighbors
+def NN(trainData, testData, trainTarget, testTarget):
+    nn_clf = KNeighborsClassifier()
+    nn_clf.fit(trainData, trainTarget)
+    nn_acc = nn_clf.score(testData, testTarget)
+    nn_acc = "%.4f" % nn_acc
+    print("Accuracy of Nearest Neighbors: " + str(nn_acc))
+    predictions = nn_clf.predict(testData)
+    print(classification_report(testTarget, predictions))
+    p = np.random.permutation(len(testData))
+    p = p[:20]
+    pltImages(testData[p].reshape(-1, 28, 28), predictions[p])
 
-print(classification_report(testTarget, predictions))
+mnist = fetch_mldata('MNIST original')
+mnData = mnist['data']
+mnTarget = mnist['target']
+mnTarget = mnTarget.astype("int32")
+mnData = mnData / 255.0
+mnData.min(), mnData.max()
+trainData, testData, trainTarget, testTarget = train_test_split(mnData, mnTarget)
+trainData.shape, testData.shape
 
-p = np.random.permutation(len(testData))
-p = p[:20]
-pltImages(testData[p].reshape(-1, 28, 28), predictions[p])
+naiveBayes(trainData, testData, trainTarget, testTarget)
+SVM(trainData, testData, trainTarget, testTarget)
+NN(trainData, testData, trainTarget, testTarget)
+
